99e096c8b4dc6e4d482fd89fb4483c3efc645cb5
Tucker Rosebrock <h.rosebrock@gmail.com>
2016-08-10 15:07:28 -0400

    added if statement to check for matching cards

--- starter-code/main.js
+++ starter-code/main.js
@@ -4,3 +4,24 @@ var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
+
+var firstCard = cardThree;
+var secondCard = cardFour;
+
+if (firstCard === 'queen' && secondCard === 'queen') {
+	alert("You found a match!")
+} else if (firstCard === 'king' && secondCard === 'king') {
+	alert("You found a match!")
+} else {
+	alert("Sorry, try again.")
+}
+
+
+// is there a reason why I can't make things a little quicker and just do this?
+
+
+// if (firstCard === secondCard) {
+// 	console.log("You found a match!") 
+// } else {
+// 	console.log("Sorry, try again.")
+// }
