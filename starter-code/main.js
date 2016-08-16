4275af93efea94fd00f673f147ae06d471939a81
Tucker Rosebrock <h.rosebrock@gmail.com>
2016-08-16 12:39:09 -0400

    made createBoard() function and updated HTML per instructions from ch 10 assignment

--- starter-code/main.js
+++ starter-code/main.js
@@ -5,20 +5,42 @@ var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
 
-var firstCard = cardThree;
-var secondCard = cardFour;
+// sets element with id 'game-board' to variable 'gameBoard'
+var gameBoard = document.getElementById('game-board');
 
-if (firstCard === 'queen' && secondCard === 'queen') {
-	alert("You found a match!")
-} else if (firstCard === 'king' && secondCard === 'king') {
-	alert("You found a match!")
-} else {
-	alert("Sorry, try again.")
-}
+// function to create HTML for each card and appends them to div with class 'board'
+var createBoard = function() {
+	
+	// for loop that executes 4 times, giving us 4 cards
+	for (var i = 0; i < 4; i++) {
 
+		// makes a new empty div element & sets it to variable 'card'
+		var card = document.createElement('div');
+		
+		// gives newly created div element a class of 'card'
+		card.className = 'card';
+
+		// appends that new card element to the 'board' div
+		document.querySelector('.board').appendChild(card);
+	}
+};
+
+//executes the function and makes our gameboard!
+createBoard();
+
+
+// if-statement from previous exercise
+
+
+// if (firstCard === 'queen' && secondCard === 'queen') {
+// 	alert("You found a match!")
+// } else if (firstCard === 'king' && secondCard === 'king') {
+// 	alert("You found a match!")
+// } else {
+// 	alert("Sorry, try again.")
+// }
 
 // is there a reason why I can't make things a little quicker and just do this?
-
 
 // if (firstCard === secondCard) {
 // 	console.log("You found a match!") 
